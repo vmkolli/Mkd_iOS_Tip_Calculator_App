f498285dd151c95581be977eb0da0c9e46a3d034
vinkol <vinkol@yahoo-inc.com>
2015-01-17 19:18:10 -0800

    Initial Commit

---
+++ MkdTipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MkdTipCalculator
+//
+//  Created by Vinayakumar Kolli on 1/17/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MkdTipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MkdTipCalculator
+//
+//  Created by Vinayakumar Kolli on 1/17/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
